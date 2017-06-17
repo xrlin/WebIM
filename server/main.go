ee2f36f982b2d54231e7c035b75d9da8f1245fe5
xrlin <xrlin@m.scnu.edu.cn>
2017-06-17 22:29:57 +0800

    feat(server): Add CORS middleware .

--- server/main.go
+++ server/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/xrlin/WebIM/server/controllers"
 	"github.com/gin-gonic/gin"
+	"github.com/xrlin/WebIM/server/middlewares"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(middlewares.CORS())
 	api := router.Group("/api")
 	{
 		api.POST("/user/token", controllers.UserToken)
